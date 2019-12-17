d07c488416aad2c27b8eb39d588fb1bebf7f8704
Khoi Nguyen <i@mkhoi.com>
2019-12-17 13:14:01 +0700

    fix puma in gemfile

--- template.rb
+++ template.rb
@@ -92,7 +92,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git :clone => [
         "--quiet",
-        "https://github.com/astrocket/rails-template",
+        "https://github.com/tfx/rails-template",
         tempdir
     ].map(&:shellescape).join(" ")
   else
